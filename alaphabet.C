2ca1f50673a9b4f6e1eece4651c2334956a2ab54
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-01-22 16:40:22 +0530

    alaphabet.C

---
+++ alaphabet.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("enter character");
+if("%c",&c);
+if((c>='a'&&c<='z')//(c>='4'&&c<='z'))
+printf("%c is alaphabet",c);
+else
+printf("%c is not an alphabet",c);
+return 0;
+}
