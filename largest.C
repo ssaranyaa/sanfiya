48aeb3a4a598acec8e27ce36cd2edf9ee3192683
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-01-22 16:47:37 +0530

    largest.C

---
+++ largest.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter three numbers\n");
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+{
+printf("\n %d is larger than %d and %d",&a&b&c);
+}
+if((b>a)&&(b>c))
+{
+printf("\n 5d is larger than %d and %d,&b&a&c);
+}
+if9(c>a0&&(c>b))
+{
+printf("\n %d is larger than %d and %d",&c&b&a);
+}
+return(0);
+}
