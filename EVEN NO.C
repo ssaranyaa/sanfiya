ea817c17591b9d8ac6d5303a8ed8d5d3ae1520e2
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-01-24 16:37:52 +0530

    EVEN NO.C

---
+++ EVEN NO.C
@@ -0,0 +1,15 @@
+#include<stdio.h.
+int main()
+[
+int f1,f2,rem,i;
+printf("first number");
+scanf("%d',&f1);
+printf("second number");
+scanf("%d",&f2);
+printf("the even number between %d and 5d",f1,f10;
+for(i=f1;i<=f2;++i)
+{
+rem=i%2;
+if(rem==0)
+printf("\n%d",i);
+}
