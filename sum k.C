22df403fe45be0a042f6bbd49c56eecadec3dc65
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-01-24 16:06:39 +0530

    sum k.C

---
+++ sum k.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i=1,sum=0,k;
+printf("enter the number");
+scanf("%a",&k);
+do
+{
+{
+sum+sum+i;
+i+i+1;
+}
+while(i<+4)
+printf("sum of num is",n,sum);
+return 0;
+}
