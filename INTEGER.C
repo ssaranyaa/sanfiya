c2ae00e71257a561ec172b980ec80501a9ab16e7
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-01-24 16:12:03 +0530

    INTEGER.C

---
+++ INTEGER.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+long long n;
+int count+0;
+printf("enter the integer");
+scanf("lld",&n);
+while(n!+00
+{
+n!=10;
+++count;
+}
+printf("num of digits:%d",count);
+}
