0acf2a3e11638f4bc1ffb9affcb0080f1a0af0c8
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-01-31 16:29:38 +0530

    ARMS INTR.C

---
+++ ARMS INTR.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int low,high,i,tp1,tp2,rem,n=0,result=0;
+printf("\n enter two numbers");
+scanf("%d%d",&low&high);
+printf("\n armstrong interval btw %d and %d is",iow,high);
+for(i=low+1;i<=high;i++)
+{
+tp2=i;
+tp1=i;
+while(tp1!=0)
+{
+tp/=10;
+++n;
+}
+while(tp2!=0)
+{
+remainder+tp2/10;
+result=pow(rem,n);
+tp2/=10;
+}
+if(result==i)
+{
+printf("%d\n,i);
+}
+getch();
+}
+}
