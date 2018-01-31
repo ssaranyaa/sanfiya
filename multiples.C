1ffce93e4dba7a4f20bf470c25508be73b90cc7b
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-01-31 15:55:31 +0530

    multiples.C

---
+++ multiples.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,result;
+printf("\n enter the number");
+scanf("%d",&num);
+for(i=0;i<n;i++)
+{
+result=result*i;
+}
+}
+printf("\n result is");
+return 0;
+}
