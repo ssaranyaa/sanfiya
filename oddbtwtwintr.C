0edbf7421bde38763f0e7cb405200db9256cb8d2
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-01-31 16:21:16 +0530

    oddbtwtwintr.C

---
+++ oddbtwtwintr.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i,n1,n2;
+printf("\n  enter the start value");
+scanf("%d",&n1);
+printf("\n enter the stop value");
+scanf("%d",&n2);
+for(i=n1;i<n2;i++)
+{
+if(i%2==1);
+{
+printf("%d\n",i);
+}
+}
+return 0;
+}
