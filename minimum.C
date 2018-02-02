6132262c62fd9ffcb97720cd35e39a0143273c34
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-02 15:41:19 +0530

    minimun.C

---
+++ minimum.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int array[100],minimum,size,location=1;
+printf(" enter no of element in array\n");
+scanf("%d",&size);
+printf("enter %d integers\n",size);
+for(c=0;c<size;c++)
+scanf("%d",&array[c]);
+min=arr(0);
+for(c=1;c<size;c++)
+{
+if(arr[c]<min)
+{
+min=arr[c];
+location=c+1;
+}
+}
+printf("\n element pre in location%d&its value is %d\n",location,min);
+return 0;
+}
