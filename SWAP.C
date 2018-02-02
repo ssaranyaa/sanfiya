a814094ea109335c906043e4fc134cb8ce0dd13f
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-02 16:51:57 +0530

    SWAP.C

---
+++ SWAP.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{                                          
+int a=10,b=20;
+clrscr();
+printf("\n before swapping a=%d,b=%d",a,b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("\n after swap a=%d,b=%d,a,b);
+getch();
+}
