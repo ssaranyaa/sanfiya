0ce0cb6d7efe2a41c2506d955f125ca8cef55981
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-02 16:42:12 +0530

    strlength.C

---
+++ strlength.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char text[400];
+int length;
+printf("\n enter the string");
+scanf("%d",&text);
+len+str len(text);
+printf("\n the length of the string is:"len);
+return 0;
+}
