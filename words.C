5a06bca620e44242b8627b006c8905082cde5220
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-05 16:47:48 +0530

    words.C

---
+++ words.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char=a[100];
+printf("\n enter the string");
+scanf("%s",&a);
+for(i=0;i<=`15;i++)
+{
+if(a[i]==' ')
+{
+count++;
+}
+}
+printf("\n the output is:",count);
+return 0;
+}
