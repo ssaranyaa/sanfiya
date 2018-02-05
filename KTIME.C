0ff4018f4567c911e377a3634aa6fc7515faf8c2
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-05 16:04:30 +0530

    KTIME.C

---
+++ KTIME.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a[20];
+int n;
+printf("\n enter the string");
+scanf("%d",a);
+printf("\n enter the no.of times");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("%s",a);
+}
+return 0;
+}
