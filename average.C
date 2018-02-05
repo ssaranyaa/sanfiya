6efb25d39a6a58ffab50c8e87f54d6e8a2161ae7
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-05 16:33:23 +0530

    average.C

---
+++ average.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,i,a,c,sum=0;
+printf("\n enter the no of values");
+sacnf("%d",n);
+for(i=1;i<n;i++)
+{
+printf("enter the %d value":,i);
+scanf("%d",&a);
+sum=sum+a;
+}
+c=sum/n;
+printf("\n the output is:%d",c);
+return 0;
+}
