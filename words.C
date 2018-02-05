5440a6b090d1a2dd81f5eb7fd095e0a4b32cf7d5
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-05 16:56:35 +0530

    words.C

--- words.C
+++ words.C
@@ -2,15 +2,17 @@
 int main()
 {
 char=a[100];
+int i=0,count=1;
 printf("\n enter the string");
 scanf("%s",&a);
 for(i=0;i<=`15;i++)
 {
 if(a[i]==' ')
 {
-count++;
+count=count+1;
 }
+  i++;
 }
-printf("\n the output is:",count);
+printf("\n the output is:%d",count);
 return 0;
 }
