c1b981d3474e944757b78f9551d2cc8f8dc028f0
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-09 16:39:03 +0530

    binary.C

---
+++ binary.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int i,count;
+char a[100];
+printf("\n enter the number:");
+for(i=0;i<a[100];i++)
+{
+scanf("%d",a[i]);
+}
+for(i=0;i<a[100];i++)
+{
+if(a[i]==0\\a[i]==1)
+{
+if(count=0)
+{
+printf("\n",yes);
+}
+else
+{
+printf("\n",no);
+}
+}
+}
+return 0;
+}
