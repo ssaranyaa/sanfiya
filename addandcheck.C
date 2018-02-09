7703d8e7dacfb50a9c176022fb7dc0ff1bf6193c
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-09 16:46:59 +0530

    addandcheck.C

---
+++ addandcheck.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("\n enter the two numbers:");
+scanf("%d %d",&a,&b);
+c=a+b;
+if(c%2==0)
+{
+printf("%d",the difference is even");
+}
+else
+{
+printf("%d",the difference is odd);
+}
+return 0;
+}
