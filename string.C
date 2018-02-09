3de9f34e19eb7625e2c2560661759b5df0c95969
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-09 15:45:49 +0530

    string.C

---
+++ string.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[i];
+int i,k;
+printf("\n enter the string:");
+scaf("%s",&a[i]);
+printf("\n enter the limit");
+scaf("%d",&k);
+for(i=0;i<k;i++)
+{
+printf("\n",a[i]);
+}
+return 0;
+}
