bf12a0e79b1004aba4ba5dfc6f308ee8c3396965
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-12 16:38:56 +0530

    numericstr12.C

---
+++ numericstr12.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int sum=0,i,a;
+char str[100];
+printf("\n enter the string:");
+gets(str);
+a=strlen(str);
+for(i=0;i<=a;i++)
+{
+if(str[i]=='1'||str[i]=='2'||str[i]=='3'||str[i]=='4'||str[i]=='5'||str[i]=='6'||str[i]=='7'||str[i]=='8'||str[i]=='9')
+sum=sum+1:
+}
+printf("\n numerical string");
+getch();
+}
