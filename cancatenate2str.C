25221893338e168829ee3812052a29ecc749a484
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-12 15:57:59 +0530

    cancatenate2str.C

---
+++ cancatenate2str.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[10];
+char b[20];
+int i,j;
+printf("\n enter the string:");
+scanf("%d",&a[i]);
+printf("\n enter the second string:");
+scanf("%d",&b[j]);
+if(a[i]=='\0')
+{
+for(j=0;j!='\0';j++)
+{
+a[i]=b[j];
+}
+}
+getch();
+}
