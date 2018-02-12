8ad4833e8f89d8c97b23e0d084e67da9f6d1b56e
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-12 16:05:27 +0530

    notoprintwords.C

---
+++ notoprintwords.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\n enter the number:");
+scanf("%d",&a);
+switch(a)
+{
+case1:printf("one");
+break;
+case2:printf("two");
+break;
+case3:printf("three");
+break;
+case4:printf("four");
+break;
+case5:printf("five");
+}
+}
