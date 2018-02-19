a3e2ffa9004cc8c775104b34cc35ef59d1c53c9b
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-19 16:10:35 +0530

    numbtw2no19.C

---
+++ numbtw2no19.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z;
+printf("\n enter the number:");
+scanf("%d",&a);
+printf("\n enter the left & right num behind that num:");
+scanf("%d %d",&y,&z);
+if(x>y&&x<z)
+{
+printf("yes\n);
+}
+else
+{
+printf("no\n");
+}
+return 0;
+}
