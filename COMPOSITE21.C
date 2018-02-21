f0f9d800218668f3f7bef463297d6084b1e00e39
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-21 16:04:24 +0530

    COMPOSITE21.C

---
+++ COMPOSITE21.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int a,i,n;
+printf("\n enter the number:");
+sacnf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+a=1;
+break;
+}
+}
+i(a==0)
+{
+printf("\n prime number");
+}
+else
+{
+printf("\n composite number");
+}
+return 0;
+}
