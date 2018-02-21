e6661414f17bb7c32a38262e1c7af99f9cdc2881
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-21 15:51:31 +0530

    VOWEL21.C

---
+++ VOWEL21.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char c[i]="ice";
+int i=0;
+if(c[i]=='a'||c[i]=='e'||c[i]=='i'||c[i]=='o'||c[i]=='u'||c[i]=='A'||c[i]=='E'||c[i]=='I'||c[i]=='O'||c[i]=='U')
+{
+printf("yes\n");
+}
+else
+{
+printf("no\n");
+}
+return 0;
+}
