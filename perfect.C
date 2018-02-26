1a12e4a4a93e13eaa9f937c58a29f95eae4f36d7
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-26 03:05:08 -0800

    perfect.C

---
+++ perfect.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,i;
+printf("enter a and b:");
+scanf("%d %d",&a,&b);
+c=a*b;
+for(i=0;i<c;i++)
+{
+if(c=(i*i))
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+return 0;
+}
