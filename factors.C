40923428ccccc0f822fb0e7afe7360442869dab4
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-26 03:15:09 -0800

    factors.C

---
+++ factors.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n;
+printf("enter the number:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+printf("\n",i);
+}
+getch();
+}
+}
