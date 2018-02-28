3450b26e9d741eb27c27dd6171068345b33e7c91
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-28 15:56:34 +0530

    FIBOSERIES28.C

---
+++ FIBOSEREIES28.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a=0,b=1,c;
+printf("\n enter the limit:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+c=a+b;
+a=b;
+b=c;
+printf("\n FIBO SERUES IS);
+}
+getch();
+}
