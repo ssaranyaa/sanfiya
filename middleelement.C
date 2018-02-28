2a43c9ab9b3eef4f3578e48c0e83a44267987b39
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-02-28 15:45:22 +0530

    middleelement.C

---
+++ middleelement.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,1;
+chara[5]="hello"
+1=strlen(a);
+if(1%2==0)
+{
+a[1/2]='*';
+}
+else
+{
+a[(1-1)/2]='*';
+}
+for(i=0;a[i]!='\0';i++)
+{
+printf("\n tne middle element is %c",a[i]);
+}
+return 0;
+}
