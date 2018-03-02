6a720ba2c240d5fd944cfa2f8fb4bade298245f3
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-03-02 16:38:17 +0530

    MAX39.C

---
+++ MAX39.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,max;
+char s[10];
+max=s[0];
+for(i=0;i<=10;i++)
+{
+scanf("%s",&s);
+}
+if(s[i]>max)
+{
+max=a[i];
+printf("\n the max is",a);
+}
+getch();
+}
