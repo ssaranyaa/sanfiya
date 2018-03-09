5cd70fa5311a42813d306790107ddceb573945e4
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-03-09 02:27:47 -0800

    COMPOSITE96.C

---
+++ COMPOSITE96.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int i,n,s;
+printf("\n enter the number:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+for(n%i==0)
+{
+s++;
+}
+}
+if(c==2)
+{
+printf("prime\n");
+}
+else
+{
+printf("composite\n");
+}
+return 0;
+}
