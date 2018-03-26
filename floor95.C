cb87a35d99cd99175bbcaeffc18b1a2d112ba3b8
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-03-26 15:53:49 +0530

    floor95.C

---
+++ floor95.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int s,p,t,r;
+printf("\n enter the values to calculate:");
+scanf("%d%d%d",&p,&t,&r);
+s=(p*t*r)/100;
+printf("\n floor value is:%d",s);
+return 0;
+}
